8982ceb8ee9201e1967ce96a9be235cbffedb43d
Juan Pablo Gaviria Franco <francopablo155@gmail.com>
2025-04-02 21:21:42 -0500

    commit clase Event

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -15,3 +15,33 @@ class Reminder:
 
     def __str__(self):
         return f"Reminder on {self.date_time} of type {self.type}"
+
+
+@dataclass()
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    reminders: list[Reminder] = field(default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
+
+    def add_reminder(self, message: str, remind_at: time) -> None:
+        reminder = Reminder(message=message, remind_at=remind_at)
+        self.reminders.append(reminder)
+
+    def delete_reminder(self, reminder_index: int) -> None:
+        if 0 <= reminder_index < len(self.reminders):
+            del self.reminders[reminder_index]
+        else:
+            reminder_not_found_error()
+
+    def __str__(self) -> str:
+        return (f"ID: {self.id}\n"
+                f"Event title: {self.title}\n"
+                f"Description: {self.description}\n"
+                f"Time: {self.start_at} - {self.end_at}")
+
+
+
