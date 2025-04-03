d749dbdfcdd2999ed9bd08314751305c6564a20d
Juan Pablo Gaviria Franco <francopablo155@gmail.com>
2025-04-02 21:39:16 -0500

    commit clase day

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -90,6 +90,27 @@ class Day:
                 current_minute = 0
                 current_hour += 1
 
+    def delete_event(self, event_id: str):
+        deleted = False
+        for slot, saved_id in self.slots.items():
+            if saved_id == event_id:
+                self.slots[slot] = None
+                deleted = True
+        if not deleted:
+            event_not_found_error()
+
+    def update_event(self, event_id: str, start_at: time, end_at: time):
+        for slot in self.slots:
+            if self.slots[slot] == event_id:
+                self.slots[slot] = None
+
+        for slot in self.slots:
+            if start_at <= slot < end_at:
+                if self.slots[slot]:
+                    slot_not_available_error()
+                else:
+                    self.slots[slot] = event_id
+
 
 
 
